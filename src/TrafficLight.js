9a99786055956b7115fe7863929ab762406f3c48
David Folkins <davidf@EXS-Programmer11.local>
2021-06-02 11:10:51 -0400

    install material ui, add boiler plate css

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -1,7 +1,28 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  container: {
+    backgroundColor: "darkgreen"
+  },
+  trafficlight: {
+    backgroundColor: "white"
+  },
+  text: {
+    color: "green"
+  },
+}));
+
 function TrafficLight() {
+  const classes = useStyles();
+
   return (
-    <div>
-      Traffic Light!
+    <div className={classes.container}>
+      <div className={classes.trafficlight}>
+        Traffic Light
+      </div>
+      <div className={classes.text}>
+        The Light is Green
+      </div>
     </div>
   );
 }
